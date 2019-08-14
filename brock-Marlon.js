213edef8215b2e0d998d4ecea98efd9908897448
Marlon Agno <mdj.agno@gmail.com>
2019-08-14 17:48:12 -0400

    added LOTR function

--- brock-Marlon.js
+++ brock-Marlon.js
@@ -121,7 +121,7 @@ const hobbitDiet = {
 console.log(hobbitDiet.meals[3]);
 */
 
-const brock = {
+/*const brock = {
   name: 'Brock',
   jobTitle: 'unemployed like a boss',
   boss: 'nobody',
@@ -155,8 +155,89 @@ for (let i = 0; i < array.length; i++){
   else{
     console.log(`${array[i].name} reports to ${array[i].boss}`);
   }
+}*/
+
+/*const codeWords = 'craft block argon meter bells brown croon droop';
+
+const cipher = {
+  a: 2,
+  b: 3,
+  c: 4,
+  d: 5,
+};
+
+function decoder(code){
+
+  let decodedWords = [];
+  let cipherKeys = Object.keys(cipher);
+  let codeArr = code.split(' ');
+
+  cipherKeys['a']
+
+  for(let i = 0; i < codeArr.length; i++){
+
+    if(codeArr[i][0] === cipherKeys[0]){
+      decodedWords.push(codeArr[i][cipher.a-1]);
+    }
+    else if(codeArr[i][0] === cipherKeys[1]){
+      decodedWords.push(codeArr[i][cipher.b-1]);
+    } 
+    else if(codeArr[i][0] === cipherKeys[2]){
+      decodedWords.push(codeArr[i][cipher.c-1]);
+    }
+    else if(codeArr[i][0] === cipherKeys[3]){
+      decodedWords.push(codeArr[i][cipher.d-1]);
+    }
+    else{
+      decodedWords.push(' ');
+    }
+  }
+  return decodedWords.join('');
+}
+
+console.log(decoder(codeWords));*/
+
+function createCharacter(name, nickname, race, origin, attack, defense){
+
+  return {
+    name,
+    nickname,
+    race,
+    origin,
+    attack,
+    defense,
+    describe(){
+      console.log(`${this.name} is a ${this.race} from ${this.origin}`)
+    },
+    evaluateFight(character){
+      return `Your opponent takes ${this.attack - character.defense} damage and you receive ${character.attack - this.defense} damage`;  
+    },    
+  };
+
 }
 
 
+const characters = [
+    createCharacter('Gandalf the White', 'gandalf', 'Wizard','Middle Earth', 10, 6),
+    createCharacter('Bilbo Baggins', 'bilbo','Hobbit','The Shire', 2, 1),
+    createCharacter('Frodo Baggins', 'frodo', 'Hobbit', 'The Shire', 3, 2),
+    createCharacter('Legolas', 'legolas','Elf','Woodland Realm', 8, 5),
+    createCharacter('Aragorn son of Arathorn', 'aragorn','Man', 'Dunnendain', 6, 8)
+];
 
 
+console.log(characters.find(character => character.nickname === 'aragorn').describe());
+
+console.log(characters.filter(character => character.race === 'Hobbit'));
+
+console.log(characters.filter(character => character.attack > 5));
+
+
+/*const gandalf = createCharacter('Gandalf the White', 'gandalf', 'Wizard','Middle Earth', 10, 6);
+const bilbo = createCharacter('Bilbo Baggins', 'bilbo','Hobbit','The Shire', 2, 1);
+const frodo = createCharacter('Frodo Baggins', 'frodo', 'Hobbit', 'The Shire', 3, 2);
+const legolas = createCharacter('Legolas', 'legolas','Elf','Woodland Realm', 8, 5);
+const aragorn = createCharacter('Aragorn son of Arathorn', 'aragorn','Man', 'Dunnendain', 6, 8);
+const 
+
+console.log(legolas.evaluateFight(aragorn));*/
